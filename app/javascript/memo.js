750b9341d8d1f633a5f5fad69edfd287a42496e6
kyamax <kisho.0125@gmail.com>
2022-09-11 14:15:13 +0900

    リクエストを処理する

---
+++ app/javascript/memo.js
@@ -0,0 +1,28 @@
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", () => {
+    // e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(FormData);
+  });
+};
+
+window.addEventListener("load", post);
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('load', post);
